a114c749bbd16f63b3355b50af83246c57c4e79d
lauromazzoni <lauromazzoni@gmail.com>
2025-09-30 15:32:28 -0300

    script update

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('resume-counter')
@@ -11,9 +12,15 @@ def lambda_handler(event, context):
         ExpressionAttributeValues={":incr": 1},
         ReturnValues="UPDATED_NEW"
     )
+
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": '{"visits": %d}' % response['Attributes']['count']
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"  # habilita CORS
+        },
+        "body": json.dumps({
+            "visits": int(response['Attributes']['count'])
+        })
     }
 
