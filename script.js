26167f9674de9b1a15c1268acf6111b73f86af56
lauromazzoni <lauromazzoni@gmail.com>
2025-09-30 16:34:15 -0300

    update script.js para remover undefined

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ async function updateCounter() {
   try {
     let response = await fetch(apiUrl);
     let data = await response.json();
-    counter.innerText = data.visits;
+
+    // o campo "visits" vem dentro de "body" como string JSON
+    let parsedBody = JSON.parse(data.body);
+
+    counter.innerText = parsedBody.visits;
   } catch (err) {
-    console.error("Erro:", err);
+    console.error("Erro ao buscar contador:", err);
     counter.innerText = "N/A";
   }
 }
