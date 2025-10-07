315e143ba75cdd235348517a8be52645017929b5
lauromazzoni <lauromazzoni@gmail.com>
2025-10-07 13:45:20 -0300

    lambda e versão do python

--- lambda_function.py
+++ lambda_function.py
@@ -16,3 +16,38 @@ def lambda_handler(event, context):
 
     visits = int(response["Attributes"]["visits"])
     return {"visits": visits}
+
+
+
+
+
+
+
+
+
+# import boto3
+# import json
+
+# dynamodb = boto3.resource('dynamodb')
+# table = dynamodb.Table('resume-counter')
+
+# def lambda_handler(event, context):
+#     response = table.update_item(
+#         Key={'id': 'visits'},
+#         UpdateExpression="ADD #count :incr",
+#         ExpressionAttributeNames={"#count": "count"},
+#         ExpressionAttributeValues={":incr": 1},
+#         ReturnValues="UPDATED_NEW"
+#     )
+
+#     return {
+#         "statusCode": 200,
+#         "headers": {
+#             "Content-Type": "application/json",
+#             "Access-Control-Allow-Origin": "*"  # habilita CORS
+#         },
+#         "body": json.dumps({
+#             "visits": int(response['Attributes']['count'])
+#         })
+#     }
+
