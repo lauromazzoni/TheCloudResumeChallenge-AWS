87f558df7ae5f97f73e2140f91f0cc64054599fd
lauromazzoni <lauromazzoni@gmail.com>
2025-10-07 21:17:24 -0300

    teste script

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const counter = document.getElementById("visits");
 const API_URL = "https://m3ezj6hkih.execute-api.us-east-1.amazonaws.com/prod/counter";
 
-
 async function updateCounter() {
   try {
-    let response = await fetch(API_URL);
-    let data = await response.json();
+    const response = await fetch(API_URL);
+    const data = await response.json();
 
-    // o campo "visits" vem dentro de "body" como string JSON
-    let parsedBody = JSON.parse(data.body);
-
-    counter.innerText = parsedBody.visits;
+    // Aceita tanto body JSON quanto resposta direta
+    const parsed = typeof data.body === "string" ? JSON.parse(data.body) : data;
+    counter.innerText = parsed.visits ?? "N/A";
   } catch (err) {
     console.error("Erro ao buscar contador:", err);
     counter.innerText = "N/A";
@@ -18,4 +16,28 @@ async function updateCounter() {
 }
 
 updateCounter();
+
+
+
+
+// const counter = document.getElementById("visits");
+// const API_URL = "https://m3ezj6hkih.execute-api.us-east-1.amazonaws.com/prod/counter";
+
+
+// async function updateCounter() {
+//   try {
+//     let response = await fetch(API_URL);
+//     let data = await response.json();
+
+//     // o campo "visits" vem dentro de "body" como string JSON
+//     let parsedBody = JSON.parse(data.body);
+
+//     counter.innerText = parsedBody.visits;
+//   } catch (err) {
+//     console.error("Erro ao buscar contador:", err);
+//     counter.innerText = "N/A";
+//   }
+// }
+
+// updateCounter();
 
