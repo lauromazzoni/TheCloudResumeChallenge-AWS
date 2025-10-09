7cddbbbb473deb0ce7f9948626cfa8d4d6f53ee4
lauromazzoni <lauromazzoni@gmail.com>
2025-10-09 19:08:21 -0300

    apigateway output update

--- terraform/modules/apigateway/outputs.tf
+++ terraform/modules/apigateway/outputs.tf
@@ -2,3 +2,13 @@ output "api_invoke_url" {
   description = "URL base da API Gateway"
   value       = "https://${aws_api_gateway_rest_api.this.id}.execute-api.${data.aws_region.current.id}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}"
 }
+
+output "api_id" {
+  description = "ID da API Gateway criada"
+  value       = aws_api_gateway_rest_api.this.id
+}
+
+output "stage_name" {
+  description = "Nome do stage da API Gateway"
+  value       = aws_api_gateway_stage.stage.stage_name
+}
